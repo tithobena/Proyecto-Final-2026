7be24c5bbad96c60a3b57e2b70eb05b177ea32de
Titho2 <abenasayag@pioix.edu.ar>
2026-09-25 12:51:09 +0000

    modified:   EsclavoV1.1.ino

--- EsclavoV1.1.ino
+++ EsclavoV1.1.ino
@@ -131,7 +131,7 @@ void setup() {
 
 void loop() {
   juegoIncia = ULTRASonicJbCOMPANY();
-  while(juegoIncia){
+  while(juegoIncia || ){
   //se fija si hay serial (cereal cremoso con yougr la serenisima)
   if (Serial.available() > 0)  {
     pepe = Serial.read();
@@ -165,6 +165,6 @@ void loop() {
     empiezalobueno = true;
     }
   }
-
+  }
 }
 
